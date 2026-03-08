fe646424d0b47018c58e722d876c0b339628455f
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-03-08 17:02:52 +0100

    Update memory.py

--- memory.py
+++ memory.py
@@ -3,19 +3,25 @@ import os
 
 DB = "memory.json"
 
-def load_memory():
+def load():
     if not os.path.exists(DB):
         return {}
     with open(DB, "r") as f:
         return json.load(f)
 
-def save_memory(data):
+def save(data):
     with open(DB, "w") as f:
         json.dump(data, f)
 
-def add_message(user, message, reply):
-    data = load_memory()
+def add_message(user, msg, reply):
+    data = load()
+
     if user not in data:
         data[user] = []
-    data[user].append({"msg": message, "reply": reply})
-    save_memory(data)
+
+    data[user].append({
+        "message": msg,
+        "reply": reply
+    })
+
+    save(data)
